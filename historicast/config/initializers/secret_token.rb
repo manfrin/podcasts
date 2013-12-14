637e95dfa994f2be95e748c20269965e08bdc7fa
Mike Manfrin <manfrin@gmail.com>
2013-12-14 12:48:04 -0800

    Initial rails 4.0.2 install.

---
+++ historicast/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Historicast::Application.config.secret_key_base = '7031805bbcead799a2471ef0a79330e6d0dce6bb62752e3aa632a98081e2e5b6c054ebc26ca4eae04b7c78c719bb9725cff2a700c6407a6b78bedf936549e0dd'
